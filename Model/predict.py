ea650b252840d03b31a87bd34e8a9fcf86f753e7
Micheal Oluwafemi Adeniyi <160871523+OfemiAdeniyi@users.noreply.github.com>
2026-02-04 12:39:37 +0100

    Update predict.py

--- Model/predict.py
+++ Model/predict.py
@@ -5,15 +5,11 @@ import pandas as pd
 
 MODEL_VERSION = "1.0.0"
 
-REDUCED_MODEL_URL = "https://drive.google.com/uc?id=XXX&export=download"
-THRESHOLD_URL = "https://drive.google.com/uc?id=YYY&export=download"
+REDUCED_MODEL_URL = "https://drive.google.com/uc?export=download&id=1o0dPggLeTrdVy7dESmsvP6IDk_Q5SeGF"
+THRESHOLD_URL = "https://drive.google.com/uc?export=download&id=1poS0OD5Z6lhl1pfgg1AyG_GmHGEweYu8"
 
 MODEL_PATH = "reduced_rf_model.pkl"
 THRESHOLD_PATH = "screening_threshold.pkl"
-
-model = None
-threshold = None
-
 
 def download_file(url: str, path: str):
     if not os.path.exists(path):
@@ -22,25 +18,20 @@ def download_file(url: str, path: str):
         response.raise_for_status()
         with open(path, "wb") as f:
             f.write(response.content)
-        print(f"{path} downloaded successfully")
-
+        print(f"{path} downloaded")
 
 def load_artifacts():
-    global model, threshold
-
     download_file(REDUCED_MODEL_URL, MODEL_PATH)
     download_file(THRESHOLD_URL, THRESHOLD_PATH)
 
     model = joblib.load(MODEL_PATH)
     threshold = joblib.load(THRESHOLD_PATH)
 
-    print("Model and threshold loaded successfully")
+    return model, threshold
 
+model, threshold = load_artifacts()
 
 def predict_output(input_dict: dict) -> float:
-    if model is None:
-        raise RuntimeError("Model not loaded")
-
     df = pd.DataFrame([input_dict])
     prob = model.predict_proba(df)[:, 1][0]
     return prob
