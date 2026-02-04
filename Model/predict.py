d1cf783cb8499b71fc4145cbe7bbf770001af3f8
Micheal Oluwafemi Adeniyi <160871523+OfemiAdeniyi@users.noreply.github.com>
2026-02-04 11:58:10 +0100

    Update predict.py

--- Model/predict.py
+++ Model/predict.py
@@ -1,19 +1,31 @@
-import pickle
+import os
+import requests
+import joblib
 import pandas as pd
 
-# Load trained screening model
-with open("Model/rf_screening_model.pkl", "rb") as f:
-    model = pickle.load(f)
+MODEL_VERSION = "1.0.0"
 
-# Load screening threshold
-with open("Model/screening_threshold.pkl", "rb") as f:
-    threshold = pickle.load(f)
+REDUCED_MODEL_URL = "https://drive.google.com/uc?id=XXX&export=download"
+THRESHOLD_URL = "https://drive.google.com/uc?id=YYY&export=download"
 
-#MLFlow
-MODEL_VERSION = '1.0.0'
+MODEL_PATH = "reduced_rf_model.pkl"
+THRESHOLD_PATH = "screening_threshold.pkl"
 
+def download_file(url, path):
+    if not os.path.exists(path):
+        response = requests.get(url)
+        response.raise_for_status()
+        with open(path, "wb") as f:
+            f.write(response.content)
 
-def predict_output(DiabetesScreeningInput: dict):
-    input_df = pd.DataFrame([DiabetesScreeningInput])
-    prob = model.predict_proba(input_df)[:, 1][0]
+download_file(REDUCED_MODEL_URL, MODEL_PATH)
+download_file(THRESHOLD_URL, THRESHOLD_PATH)
+
+model = joblib.load(MODEL_PATH)
+threshold = joblib.load(THRESHOLD_PATH)
+
+def predict_output(input_dict: dict) -> float:
+    df = pd.DataFrame([input_dict])
+    prob = model.predict_proba(df)[:, 1][0]
     return prob
+
