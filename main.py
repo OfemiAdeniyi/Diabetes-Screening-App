a77269ee2e6ca91c1ba581a97bab2a5bb60264a8
Micheal Oluwafemi Adeniyi <160871523+OfemiAdeniyi@users.noreply.github.com>
2026-02-04 12:05:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,62 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 from schema.DiabetesScreeningInput import DiabetesScreeningInput
 from schema.prediction_response import DiabetesScreeningResponse
-from Model.predict import predict_output, model, MODEL_VERSION, threshold
+from Model.predict import (
+    predict_output,
+    load_artifacts,
+    MODEL_VERSION,
+    model,
+    threshold,
+)
+
+from contextlib import asynccontextmanager
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    print("Starting application...")
+    load_artifacts()
+    yield
+    # Shutdown (optional cleanup)
 
-    
 
-# Human Readable
-@app.get('/')
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/")
 def home():
-    return{'message':'Diabetes Screening API'}
+    return {"message": "Diabetes Screening API"}
 
-# Machine readable
-@app.get('/health')
+
+@app.get("/health")
 def health_check():
-    return{
-        'status':'OK',
-        'Version': MODEL_VERSION,
-        'model_loaded': model is not None
+    return {
+        "status": "OK",
+        "version": MODEL_VERSION,
+        "model_loaded": model is not None,
+        "threshold_loaded": threshold is not None,
     }
 
 
-@app.post("/screen-diabetes", response_model= DiabetesScreeningResponse)
-def Screen_Patient_for_Diabetes(data: DiabetesScreeningInput):
-    try:
-        user_input = {
-            "age": data.age,
-            "gender": data.gender,
-            "smoking_history": data.smoking_history,
-            "bmi": data.bmi,
-            "hypertension": data.hypertension_bin,
-            "heart_disease": data.heart_disease_bin
-        }
+@app.post("/screen-diabetes", response_model=DiabetesScreeningResponse)
+def screen_patient_for_diabetes(data: DiabetesScreeningInput):
 
-        prob = predict_output(user_input)
+    user_input = {
+        "age": data.age,
+        "gender": data.gender,
+        "smoking_history": data.smoking_history,
+        "bmi": data.bmi,
+        "hypertension": data.hypertension_bin,
+        "heart_disease": data.heart_disease_bin,
+    }
 
-        screening_result = "High Risk" if prob >= threshold else "Low Risk"
+    prob = predict_output(user_input)
+    screening_result = "High Risk" if prob >= threshold else "Low Risk"
 
-        return JSONResponse(
-    status_code=200,
-    content={
+    return {
         "model_version": MODEL_VERSION,
         "diabetes_risk_probability": round(float(prob), 3),
         "screening_result": screening_result,
-        "screening_threshold": round(float(threshold), 3)
+        "screening_threshold": round(float(threshold), 3),
     }
-)
-
-    except Exception as e:
-        return JSONResponse(status_code=500,content={"error": str(e)})
